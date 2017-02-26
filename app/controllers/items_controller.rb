9108f88592c47416b1be363e84dc731dae9f8726
Justin Wei <wei.justin.a@gmail.com>
2017-02-25 23:50:26 -0600

    fuck

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
 
   def create
-    current_account.items.create(item_params)
+    item = current_account.items.create(item_params)
+    item.checked_out = false
+    item.save
     redirect_to dashboard_path
   end
 
@@ -9,20 +11,9 @@ class ItemsController < ApplicationController
   end
 
   def checkout
-    employee = current_account.employees.find_by_name(params[:employee_name])
     item = current_account.items.find_by_name(params[:item_name])
-
-    if item.checked_out
-      item.checked_out = false
-      new_item = employee.items.new
-      new_item = item.clone
-    else
-      item.checked_out = true
-      employee.items.find(params[:item_name]).destroy
-    end
     redirect_to dashboard_path
   end
-
 
   def item_params
     params.require(:item).permit(:name)
